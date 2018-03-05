e36713a81265facdb58fdd12152519f2e326dc4a
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2018-03-05 09:27:44 -0300

    teste dos trigger

--- consultas.sql
+++ consultas.sql
@@ -85,6 +85,13 @@ CREATE TRIGGER VERIFICA_VALOR_COTA
                 :new.cota := :old.cota;
             END IF;
         END VERIFICA_VALOR_COTA;
+
+
+/*tentativa de update de uma cota_area_volume */        
+UPDATE Cota_Area_Volume
+SET cota = 5
+WHERE id = 1;        
+        
 /
 
 
@@ -97,6 +104,12 @@ CREATE OR REPLACE TRIGGER VERIFICA_DATA_MEDICAO
                 :new.data := SYSDATE;
             END IF;
          END VERIFICA_DATA_MEDICAO;   
+
+INSERT INTO MedicaoPluviometrica VALUES(9,2,115210937);
+ /*INSERINDO UMA MEDIÇÃO COM UMA DATA INVALIDA(POSTERIOR AO DIA ATUAL)*/
+INSERT INTO DiaMedPluviometrica VALUES(100,'01/01/2019',9);         
+         
+         
 /
         
 /* 10. Liste os valores de DBO medidos para o rio Amazonas entre os dias 02/11/2017 e 02/01/2018.*/
