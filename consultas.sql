2dd1517da7750e9a848b510d6ce350c42a3ed712
mariana-mendes <mariana.silva@ccc.ufcg.edu.br>
2018-03-04 23:20:03 -0300

    triggers

--- consultas.sql
+++ consultas.sql
@@ -2,20 +2,29 @@
 
 SELECT u.nome
 FROM Usuario u
-WHERE u.idMatricula not in (
-       SELECT DISTINCT idMatricula
-       FROM Medicao_Cota_Diaria, MedicaoPluviometrica);
+WHERE u.idMatricula in(
+    SELECT u.idMatricula
+        FROM Usuario u LEFT OUTER JOIN  Medicao_Cota_Diaria mcd ON 
+             u.idMatricula = mcd.idMatricula
+            WHERE mcd.idMedicaoDiaria is NULL
+        INTERSECT 
+    SELECT u.idMatricula
+        FROM Usuario u LEFT OUTER JOIN  MedicaoPluviometrica mp ON 
+             u.idMatricula = mp.idUsuario
+            WHERE mp.idMedicao is NULL 
+);
 
 /* 2. Crie uma visão que liste os valores de chuva diários medidos para o açude de Bodocongó.*/
 
 CREATE VIEW VALORES_DIARIOS_BODOCONGO(VALORES_CHUVA) AS   
     SELECT valorChuva
-    FROM DiaMedPluviometrica diaMed, MedicaoPluviometrica med, PostoPluviometrico posto, Rio rio, Acude acude
-    WHERE   diaMed.idMedicao = med.idMedicao AND
-            med.idPostoPluviometrico = posto.idPostoPluviometrico AND
-            posto.idBacia = rio.idBacia AND
-            acude.idRio = rio.idRio AND
-            acude.nome = 'Bodocongó';
+    FROM DiaMedPluviometrica diaMed, MedicaoPluviometrica med, PostoPluviometrico posto, Contribui_Posto_Acude cpa, Acude a
+    WHERE diaMed.idMedicao = med.idMedicao AND 
+          posto.idPostoPluviometrico = med.idMedicao AND
+          cpa.idPostoPluviometrico = posto.idPostoPluviometrico AND
+          cpa.idAcude = a.idAcude AND
+          a.nome = 'Bodocongó';
+          
 
 /*3. Crie uma visão que liste os nomes de todas as estações de qualidade que já realizaram medições para rios de Pernambuco, bem como o nome do rio.*/
 
@@ -26,6 +35,8 @@ CREATE VIEW ESTACOES_RIOS_PERNAMBUCO(estacao_PE,nome_PE) AS
            pp.idBacia = b.idBacia AND
            b.idBacia = r.idBacia AND
            eq.idRio = r.idRio);
+SELECT *
+FROM VALORES_DIARIOS_BODOCONGO;
 
 /* 4. Liste os nomes dos postos pluviométricos e seus estados, agrupados pelo nome do estado.*/
 
@@ -63,6 +74,31 @@ SELECT alcalinidade AS alcalinidade_Bodocongo
     USING (idEstacaoQualidade)
 ORDER BY alcalinidade DESC;
 
+
+/*8. Faça um trigger que não permita a atualização do valor da cota de uma Cota Área Volume já existente.*/
+
+CREATE TRIGGER VERIFICA_VALOR_COTA
+    BEFORE UPDATE ON Cota_Area_Volume 
+    FOR EACH ROW 
+        BEGIN
+            IF (:old.cota is not NULL) THEN
+                :new.cota := :old.cota;
+            END IF;
+        END VERIFICA_VALOR_COTA;
+/
+
+
+/*9. Faça um trigger que, ao tentar inserir uma medição pluviométrica de um posto com uma data posterior ao dia atual, seja feita a inserção usando a data atual.*/
+CREATE OR REPLACE TRIGGER VERIFICA_DATA_MEDICAO
+    BEFORE INSERT OR UPDATE ON DiaMedPluviometrica
+    FOR EACH ROW
+        BEGIN
+            IF(:new.data > SYSDATE) THEN
+                :new.data := SYSDATE;
+            END IF;
+         END VERIFICA_DATA_MEDICAO;   
+/
+        
 /* 10. Liste os valores de DBO medidos para o rio Amazonas entre os dias 02/11/2017 e 02/01/2018.*/
 
 SELECT DBO
@@ -92,7 +128,7 @@ FROM Bacia
     WHERE area = (SELECT MIN(area) 
     FROM Bacia);
 
-/* 15. Qual o nome do usuário que mais realizou medições de cotas diárias, e quantas foram? */
+/* 15. Qual o nome do usuário que mais realizou medições de cotas diárias, e quantas foram?  */
 SELECT nome, (SELECT *
     FROM (
     SELECT MAX(COUNT(idMatricula))
@@ -106,5 +142,7 @@ WHERE u.idMatricula =
     SELECT idMatricula,COUNT(idMatricula)
     FROM Medicao_Cota_Diaria
     GROUP BY idMatricula ORDER BY COUNT(idMatricula) DESC)
-where rownum = 1);   
+where rownum = 1);  
+
+
 
